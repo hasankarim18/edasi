0054478f7712ff2228853337138a48663f6c8526
hasankarim18 <hallysust@gmail.com>
2020-11-11 08:50:24 +0600

    edasi

---
+++ js/first.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#myhide').click(function(){
+		$('#kkd').hide();
+	})
+		$('#myshow').click(function(){
+		$('#kkd').show();
+	})
+});
+
+
+document.getElementById('demo').innerHTML = Math.random()*50;
